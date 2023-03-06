5dd8140cff5246d178881f86ad549609eda87c18
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-03-06 01:53:22 -0600

    add tailwind and eslint config

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+	return (
+		<header>
+			<h2 className="w-full min-h-max px-4 py-2 absolute top-0 z-30 font-inter text-2xl bg-body text-gray-200">Sapphire Music</h2>
+		</header>
+	);
+};
+
+export default Header;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+	return (
+		<aside className="w-48 min-h-screen mt-12 fixed top-0 left-0 z-30 bg-navbar">
+			<article>
+				<h1>user info</h1>
+			</article>
+		</aside>
+	);
+};
+
+export default Navbar;
